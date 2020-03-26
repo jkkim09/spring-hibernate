2d56d3198bbd5cd6d3981a70a7e422db31a0f902
jakyoungkim <jakyoung.kim@catenoid.net>
2020-03-26 14:46:42 +0900

    양방향 적용

--- src/main/java/com/hibernate/spring/entity/User.java
+++ src/main/java/com/hibernate/spring/entity/User.java
@@ -1,9 +1,12 @@
 package com.hibernate.spring.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
@@ -13,4 +16,8 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long user_number;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_number")
+	Team team;
 }
